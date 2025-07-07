0a2fea16fd1ec22bfb494d12a2b10b8978967017
Samuel Santos <samuel.santos@maximustribe.com>
2025-07-07 00:07:00 -0300

    xd

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,21 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <Script
+          src="https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js"
+          strategy="afterInteractive"
+        />
+        <div
+          id="appleid-signin"
+          data-color="black"
+          data-border="true"
+          data-type="sign in"
+        ></div>
         <Main />
         <NextScript />
       </body>
